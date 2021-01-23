dd7a6ae7b5cbdf78e7b41c5c10fec05d76438853
Brian Flieck <flieckb@KOP01-BflieckMacBookPro2016.local>
2021-01-22 20:37:58 -0500

    updated HTML with DIVs to figure out the CSS part of the exersize, started adding basic JS functions, added a test HTML file to play around, and added GIF of the project to refer to. git push

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+// var pastDiv = $("<div contenteditable='true' class='past row hour col-11' id='textarea description'>");
+var past = "past";
+var present = "present";
+var future= "future";
+// $(".description").last().addClass(past);
+$(".description").last().addClass(present);
+// $(".description").last().addClass(future);
+
+
+})
+
+
+
+
